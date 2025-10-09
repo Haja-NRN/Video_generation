b666ce799e22bbadd3a0c52877b0eac8b3d5a48b
Ranaivoson Jacques Hajanirina <hajanirina98228@gmail.com>
2025-10-09 08:02:27 +0330

    Refactor generate_video_task to streamline event loop creation and remove unnecessary comments

--- app/celery_app/task.py
+++ app/celery_app/task.py
@@ -5,15 +5,10 @@ from app.api.routes.post_generation import generate_video_with_bytez,generate_vi
 
 @celery_app.task(name="video.generate")
 def generate_video_task(payload: dict):
-    """
-    Salalalalaala
-    """
     loop = asyncio.new_event_loop()
-    print("New event loop created")
     asyncio.set_event_loop(loop)
-    if True:
-        platform = payload["platform"]
-        prompt = payload["prompt"]
-        duration=payload["duration"]
+    platform = payload["platform"]
+    prompt = payload["prompt"]
+    duration=payload["duration"]
     result = loop.run_until_complete(generate_video_general(prompt=prompt, platform=platform,duration=duration))
     return result
